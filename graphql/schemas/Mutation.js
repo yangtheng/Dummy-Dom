1f10fbe696e6e68d896e742df0a4d248cacb479b
Rebecca Tay <rebeccatay92@gmail.com>
2017-11-10 17:02:24 +0800

    10th nov stash before adding days column

--- graphql/schemas/Mutation.js
+++ graphql/schemas/Mutation.js
@@ -22,9 +22,9 @@ const Mutation = `
 
     createLocation(placeId: String!, CountryId: ID, name:String, latitude:String, longitude:String,openingHour:String,closingHour:String,address:String): Location
 
-    createActivity(ItineraryId: ID!, googlePlaceData: googlePlaceData, LocationId: ID, date: Int, loadSequence: Int, name: String, notes: String, startTime: Int, endTime: Int, cost: Int, currency: String, bookingStatus: String, bookedThrough: String, bookingConfirmation: String, attachment: String): Activity
+    createActivity(ItineraryId: ID!, googlePlaceData: googlePlaceData, LocationId: ID, date: Int, loadSequence: Int, name: String, notes: String, startTime: Int, endTime: Int, cost: Int, currency: String, bookingStatus: Boolean, bookedThrough: String, bookingConfirmation: String, attachment: String): Activity
 
-    updateActivity(id: ID!, googlePlaceData: googlePlaceData, date: Int, loadSequence: Int, name: String, notes: String, startTime: Int, endTime: Int, cost: Int, currency: String, bookingStatus: String, bookedThrough: String, bookingConfirmation: String, attachment: String): Activity
+    updateActivity(id: ID!, googlePlaceData: googlePlaceData, date: Int, loadSequence: Int, name: String, notes: String, startTime: Int, endTime: Int, cost: Int, currency: String, bookingStatus: Boolean, bookedThrough: String, bookingConfirmation: String, attachment: String): Activity
 
     deleteActivity(id:ID!): Boolean
 
